054ace4569f1b9ad34d3fec55215b677301bb24f
Tomer Levi <Tomer.Levi@Klarna.com>
2014-06-09 10:57:39 +0300

    changes routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+root :to => 'WelcomeController#index'
+
 FirstApp::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
